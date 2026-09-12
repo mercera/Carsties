1407a488933d169de408fc8bff2d3f7f17539bbf
sahanIlla96 <sahan@bluetag.no>
2026-09-12 17:51:39 +0530

    publishing locally

--- frontend/web-app/app/api/auth/signin/page.tsx
+++ frontend/web-app/app/api/auth/signin/page.tsx
@@ -1,12 +1,14 @@
 import EmptyFilter from "@/app/components/EmptyFilter";
 
-export default function Page({searchParams}: {searchParams: {callbackUrl: string}}) {
+export default async function Page({searchParams}: {searchParams: Promise<{callbackUrl: string}>}) {
+  const {callbackUrl} = await searchParams;
+  
   return (
     <EmptyFilter 
         title='You need to be logged in to do that'
         subtitle="Please click below to login"
         showLogin
-        callbackUrl={searchParams.callbackUrl}
+        callbackUrl={callbackUrl}
     />
   )
 }
